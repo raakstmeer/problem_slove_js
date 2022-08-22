5ea28c1733e3321c539837bb6f1238c2e9f5c5cd
tusharimran <raakstmeer@gmail.com>
2022-08-23 05:54:54 +0600

    sort proggraming add

--- script.js
+++ script.js
@@ -4,17 +4,32 @@ let lodoBtn = document.querySelector("#lodoButton");
 let lodoValueShow = document.querySelector("#lodoNumShow");
 
 
-lodoBtn.addEventListener("click",()=>{
-    function lodoRanNumber(min,max){
-        let intNum = Math.floor(Math.random() * (max - min + 1))+1;
+lodoBtn.addEventListener("click", () => {
+    function lodoRanNumber(min, max) {
+        let intNum = Math.floor(Math.random() * (max - min + 1)) + 1;
         return Math.abs(intNum);
     }
-    let randomLodoNumber = lodoRanNumber(1,6);
+    let randomLodoNumber = lodoRanNumber(1, 6);
     lodoValueShow.innerHTML = randomLodoNumber;
 });
 
 // lodo program end
 
 // কিভাবে আমরা আমাদের শ্রেণীকক্ষের সকলের নাম Alphabetically সাজাতে পারি ?
+
+let unsort = document.querySelector("#unsort");
+let sort = document.querySelector("#sort");
+let sortBtn = document.querySelector("#sortButton");
+
+const students = ["Tushar", "Paiya", "Atifa", "Farhad", "Arafat", "Shifat", "Tofael", "Kaifa", "Samiya", "Rohan", "Jayed Abdullah"];
+
+unsort.innerHTML = students;
+sortBtn.addEventListener("click",()=>{
+    let dosort = students.sort();
+    sort.innerHTML = dosort;
+});
+
+// sort proggram end 
+
 
 
