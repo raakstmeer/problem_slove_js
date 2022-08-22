837288ceed15d7aeb6348d9517ea465b47b51ad1
tusharimran <raakstmeer@gmail.com>
2022-08-22 20:07:59 +0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // লুডু খেলায় আমরা কিভাবে ১ থেকে ৬ সংখ্যা রেন্ডমলি প্রিন্ট করতে পারি । 
+
+let lodoBtn = document.querySelector("#lodoButton");
+let lodoValueShow = document.querySelector("#lodoNumShow");
+
+
+lodoBtn.addEventListener("click",()=>{
+    function lodoRanNumber(max){
+        let intNum = Math.floor(Math.random()*(max-1)+1);
+        return Math.abs(intNum)
+    }
+    let randomLodoNumber = lodoRanNumber(7);
+    lodoValueShow.innerHTML = randomLodoNumber;
+});
+
+// lodo program end
+
